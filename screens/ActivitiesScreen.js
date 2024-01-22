ea16b64232854d257e67bfc1ed16f389457f0db2
Ewoma Ozore <ewomaozore@gmail.com>
2024-01-22 12:13:47 +0100

    initial commit

--- screens/ActivitiesScreen.js
+++ screens/ActivitiesScreen.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity, ProgressBarAndroid } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, Image, ScrollView } from 'react-native';
+import Calendar from '../components/Calendar';
 
 const ActivitiesScreen = () => {
     const [activities, setActivities] = useState([
-        { id: '1', title: 'Reading a Board Book', progress: 0.6 },
+        { id: '1', title: 'Reading a Board Book', progress: 0.5 },
         { id: '2', title: 'Playing with Soft Toys', progress: 0.2 },
         { id: '3', title: 'Drawing with Crayons', progress: 0.8 },
         { id: '4', title: 'Singing Nursery Rhymes', progress: 0.4 },
@@ -21,24 +22,57 @@ const ActivitiesScreen = () => {
     ]);
 
     return (
-        <View className="p-4">
-            <Text className="text-2xl font-bold mb-4 text-center">Activities</Text>
-            <FlatList
-                data={activities}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => (
-                    <TouchableOpacity className="bg-white rounded p-4 mb-4 elevation-3">
-                        <Text className="text-lg mb-2 font-bold">{item.title}</Text>
-                        <ProgressBarAndroid
-                            styleAttr="Horizontal"
-                            indeterminate={false}
-                            progress={item.progress}
-                            className="h-2"
-                        />
-                    </TouchableOpacity>
-                )}
-            />
-        </View>
+        <ScrollView className="p-4">
+            <View className="flex-row justify-center mb-4">
+                <Calendar />
+            </View>
+
+            <View className="bg-white rounded p-4 mb-4 elevation-3 items-center">
+                <Image
+                    source={require('../assets/screen1.jpg')}
+                    className="w-32 h-32 rounded-full mb-2"
+                />
+                <View className="flex flex-row justify-between w-full px-5">
+                    <View className="flex-col items-center">
+                        <Text className="text-[#365486] font-bold">Height</Text>
+                        <Text className="font-extrabold text-lg">70cm</Text>
+                    </View>
+                    <View className="flex-col items-center">
+                        <Text className="text-[#365486] font-bold">Weight</Text>
+                        <Text className="font-extrabold text-lg">8kg</Text>
+                    </View>
+                    <View className="flex-col items-center">
+                        <Text className="text-[#365486] font-bold">Age</Text>
+                        <Text className="font-extrabold text-lg">9 months</Text>
+                    </View>
+                </View>
+            </View>
+
+
+            <View className="p-4">
+                <Text className="text-2xl font-bold mb-4 text-center">Activities</Text>
+                <FlatList
+                    data={activities}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity className="bg-white rounded p-4 mb-4 elevation-3 w-full">
+                            <View className="flex flex-row justify-between items-center mb-2">
+                                <Text className="text-lg font-bold">{item.title}</Text>
+                                <Text>Status: {item.progress === 1 ? 'Completed' : 'Ongoing'}</Text>
+                            </View>
+                            <View className="w-full h-2 bg-gray-300 rounded-full overflow-hidden">
+                                <View
+                                    style={[
+                                        { width: `${item.progress * 100}%`, backgroundColor: 'blue', height: '100%' },
+                                    ]}
+                                />
+                            </View>
+                        </TouchableOpacity>
+                    )}
+                />
+            </View>
+
+        </ScrollView>
     );
 };
 
--- screens/MyChildScreen.js
+++ screens/MyChildScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const MyChildScreen = () => {
     const childDetails = {
@@ -12,21 +13,24 @@ const MyChildScreen = () => {
 
     return (
         <View className="p-4 bg-gray-100 flex-1">
-            <Text className="text-3xl font-bold mb-6 text-center text-blue-700">My Child's Details</Text>
-            <View className="items-center mb-6">
-                <Image source={childDetails.picture} className="w-40 h-40 rounded-full mb-4" />
-                <Text className="text-xl font-bold text-blue-800">{childDetails.name}</Text>
+            <Text className="text-2xl font-bold mb-6 text-center text-blue-700">{childDetails.name}</Text>
+            <View className="bg-white rounded p-6 mb-4 elevation-3 items-center">
+                <Image source={childDetails.picture} className="w-40 h-40 rounded-full mb-2" />
+                <View className="flex flex-row justify-between w-full px-5">
+                    <View className="flex-col items-center">
+                        <Text className="text-[#365486] font-bold">Milestones Completed</Text>
+                        <Text className="font-extrabold text-lg">{childDetails.milestonesCompleted}</Text>
+                    </View>
+                    <View className="flex-col items-center">
+                        <Text className="text-[#365486] font-bold">Progress</Text>
+                        <Text className="font-extrabold text-lg">{childDetails.activitiesProgress}%</Text>
+                    </View>
+                </View>
             </View>
-            <Text className="text-lg mb-2 text-gray-700">
-                Age: <Text className="text-base font-normal">{childDetails.age}</Text>
-            </Text>
-            <Text className="text-lg mb-2 text-gray-700">
-                Milestones Completed:{' '}
-                <Text className="text-base font-normal text-green-700">{childDetails.milestonesCompleted}</Text>
-            </Text>
+
             <View className="mb-6">
-                <Text className="text-lg mb-2 text-gray-700">Activities Progress:</Text>
-                <View className="bg-gray-300 h-4 rounded-full overflow-hidden">
+                <Text className="text-lg mb-2 text-gray-700 text-center">Activities Progress:</Text>
+                <View className="bg-gray-300 h-3 rounded-full overflow-hidden">
                     <View
                         style={[
                             { width: `${childDetails.activitiesProgress}%`, backgroundColor: 'blue' },
@@ -35,14 +39,41 @@ const MyChildScreen = () => {
                     />
                 </View>
             </View>
-            {/* Additional Child Details */}
-            <View>
-                <Text className="text-xl font-bold mb-2 text-gray-800">Additional Details</Text>
-                {/* Add more child details here */}
-                <Text className="text-lg mb-2 text-gray-700">Favorite Toy: Teddy Bear</Text>
-                <Text className="text-lg mb-2 text-gray-700">Sleeping Habits: 10 hours a night</Text>
-                <Text className="text-lg mb-2 text-gray-700">Health Notes: No allergies</Text>
-            </View>
+
+            <ScrollView>
+                <View className="flex">
+                    <View className="flex-row justify-between">
+                        <View className="bg-white p-5 items-center w-[48%] mb-4 rounded-lg">
+                            <FontAwesome5 name="medkit" size={24} color="#365486" />
+                            <Text className="text-lg mt-2 text-gray-700">Medicine</Text>
+                        </View>
+                        <View className="bg-white p-5 items-center w-[48%] mb-4 rounded-lg">
+                            <FontAwesome5 name="dumbbell" size={24} color="#365486" />
+                            <Text className="text-lg mt-2 text-gray-700">Exercises</Text>
+                        </View>
+                    </View>
+                    <View className="flex-row justify-between">
+                        <View className="bg-white p-5 items-center w-[48%] mb-4 rounded-lg">
+                            <FontAwesome5 name="hospital" size={24} color="#365486" />
+                            <Text className="text-lg mt-2 text-gray-700">Hospitals</Text>
+                        </View>
+                        <View className="bg-white p-5 items-center w-[48%] mb-4 rounded-lg">
+                            <FontAwesome5 name="book" size={24} color="#365486" />
+                            <Text className="text-lg mt-2 text-gray-700">Articles</Text>
+                        </View>
+                    </View>
+                    <View className="flex-row justify-between">
+                        <View className="bg-white p-5 items-center w-[48%] mb-4 rounded-lg">
+                            <FontAwesome5 name="video" size={24} color="#365486" />
+                            <Text className="text-lg mt-2 text-gray-700">Videos</Text>
+                        </View>
+                        <View className="bg-white p-5 items-center w-[48%] mb-4 rounded-lg">
+                            <FontAwesome5 name="utensils" size={24} color="#365486" />
+                            <Text className="text-lg mt-2 text-gray-700">Food</Text>
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     );
 };
